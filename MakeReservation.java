6204ea7777671d58a864fb575f48672c526ffdaf
payalahuja98 <payalahuja98@users.noreply.github.com>
2018-12-11 20:24:13 -0600

    Add files via upload

--- MakeReservation.java
+++ MakeReservation.java
@@ -19,12 +19,59 @@ public class MakeReservation extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_screen);
-        //setRoom(1);
+
+        TextView countDownText = findViewById(R.id.timeLeft);
+
+        countDownText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setRoomTimer();
+            }
+        });
     }
 
-    public void setRoom(int roomNum) {
-        rooms[roomNum] = !(rooms[roomNum]);
+    /*public void setRoom() {
+        rooms[0] = !(rooms[0]);
         setRoomTimer(getApplicationContext());
+    }*/
+
+    CountDownTimer timer;
+    long timeLeftMill;
+    TextView timeLeft;
+
+    public void setRoomTimer() {
+        timeLeft = findViewById(R.id.timeLeft);
+        timer = new CountDownTimer(3600000, 1000) {
+            public void onTick(long millUntilDone) {
+                timeLeftMill = millUntilDone;
+                updateTimer();
+            }
+
+            public void onFinish() {
+                timeLeft.setText("Time's up!");
+                stopTimer();
+            }
+        }.start();
+    }
+
+    public void updateTimer() {
+        int mins = (int) timeLeftMill / 60000;
+        int sec = (int) timeLeftMill % 60000 / 1000;
+
+        String timeLeftText;
+
+        timeLeftText = mins + ":";
+
+        if (sec < 10) {
+            timeLeftText += "0";
+        } else {
+            timeLeftText += sec;
+        }
+
+        timeLeft.setText(timeLeftText);
+    }
+
+    public void stopTimer() {
+        timer.cancel();
     }
 
     public void setAllRooms() {
@@ -44,19 +91,4 @@ public class MakeReservation extends AppCompatActivity {
         return emptyRooms;
     }
 
-    protected Timer roomTimer;
-
-    public void setRoomTimer(Context context) {
-        final TextView timeLeft = (TextView) ((MakeReservation) context).findViewById(R.id.timeLeft);
-        new CountDownTimer(3600000, 0) {
-            public void onTick(long millUntilDone) {
-                long calculateTime = (millUntilDone / 60000);
-                timeLeft.setText(String.valueOf(calculateTime));
-            }
-
-            public void onFinish() {
-                timeLeft.setText("Time's up!");
-            }
-        }.start();
-    }
 }
